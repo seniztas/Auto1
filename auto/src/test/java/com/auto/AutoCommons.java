050ffe6f25d19bef7c040010a4cccdfbd3df691b
seniztas <seniz.tascioglu@outlook.com>
2017-01-25 20:42:50 +0200

    update geckodriver path

--- auto/src/test/java/com/auto/AutoCommons.java
+++ auto/src/test/java/com/auto/AutoCommons.java
@@ -6,6 +6,6 @@ public class AutoCommons {
 	
 	public  WebDriver driver;
 	public  final String baseUrl ="https://www.auto1.com/";
-	public final String geckoDriverPath = "C:\\Users\\fst\\Downloads\\geckodriver-v0.13.0-win64\\geckodriver.exe";
+	public final String geckoDriverPath = "C:\\geckodriver.exe";
 
 }
